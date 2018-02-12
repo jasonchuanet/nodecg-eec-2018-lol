d2ff02ee87641f4276d07edf88d4d0df11e69eaf
Jason Chua <jasonc1804@gmail.com>
2018-02-12 00:12:32 -0500

    Working control for lower third

--- extension/lower_third.js
+++ extension/lower_third.js
@@ -4,31 +4,53 @@ module.exports = function (nodecg) {
 	const name = nodecg.Replicant('lower_third_name', 		{defaultValue: ''});
 	const title = nodecg.Replicant('lower_third_title', 	{defaultValue: ''});
 
-	const toggle = nodecg.Replicant('lower_third_toggle', {defaultValue: false});
-	const state_3s = nodecg.Replicant('lower_third_3s', {defaultValue: false});
-	const state_5s = nodecg.Replicant('lower_third_5s', {defaultValue: false});
+
+    const toggle   = nodecg.Replicant("lower_third_toggle", {defaultValue: {active: false, disabled: false}});
+    const state_3s = nodecg.Replicant("lower_third_3s", 	{defaultValue: {active: false, disabled: false}});
+    const state_5s = nodecg.Replicant("lower_third_5s", 	{defaultValue: {active: false, disabled: false}});
 
 	const lock = nodecg.Replicant('lower_third_lock', {defaultValue: false});
 
 	const visibility = nodecg.Replicant('lower_third_vis', {defaultValue: false});
 
-    toggle.on('change', () => {
-        visibility.value = toggle.value;
-    });
+    let buttons = [state_3s, state_5s];
+
+    let timer = null;
 
     state_3s.on('change', () => {
-        display(3000);
+        if (state_3s.value.active === true) {
+            display(3500, state_3s);
+        }
     });
 
     state_5s.on('change', () => {
-        display(3000);
+        if (state_5s.value.active === true) {
+            display(5500, state_5s);
+        }
     });
 
-	async function display(for_this_long) {
-        visibility.value = true;
-        lock.value = true;
-        await sleep(for_this_long);
-        visibility.value = false;
-        lock.value = false;
-	}
+    toggle.on('change', () => {
+        visibility.value = toggle.value.active;
+        lock.value = toggle.value.active;
+        disable(buttons, toggle.value.active);
+    });
+
+	async function display(this_long, triggering_button) {
+        toggle.value.active = true;
+        toggle.value.disabled = true;
+        await sleep(this_long);
+        toggle.value.disabled = false;
+        toggle.value.active = false;
+        triggering_button.value.active = false;
+    }
+    
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function disable(array, toggle) {
+        array.forEach(function(element) {
+            element.value.disabled = toggle;
+        });
+    }
 };
