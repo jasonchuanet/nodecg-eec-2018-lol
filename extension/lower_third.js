2edba09ac71e44bd994cc55e2368b88cffc71d45
Jason Chua <jasonc1804@gmail.com>
2018-02-19 17:18:59 -0500

    Lower third animations

--- extension/lower_third.js
+++ extension/lower_third.js
@@ -16,30 +16,26 @@ module.exports = function (nodecg) {
     let buttons = [state_3s, state_5s];
 
     let timer = null;
+    let safety = null;
 
     state_3s.on('change', () => {
         if (state_3s.value.active === true) {
-            display(3500, state_3s);
-        } else {
-            timer = null;
-            active(buttons, false);
+            safety = display(3000, state_3s);
         }
     });
 
     state_5s.on('change', () => {
         if (state_5s.value.active === true) {
-            display(5500, state_5s);
-        } else {
-            timer = null;
-            active(buttons, false);
+            safety = display(5000, state_5s);
         }
     });
 
     toggle.on('change', () => {
         visibility.value = toggle.value.active;
         lock.value = toggle.value.active;
-        if (timer && toggle.value.active === false) {
-            timer = null;
+        if (safety && toggle.value.active === false) {
+            safety = null;
+            clearTimeout(timer);
             active(buttons, false);
         }
         disable(buttons, toggle.value.active);
@@ -49,13 +45,14 @@ module.exports = function (nodecg) {
         toggle.value.active = true;
         disable(buttons, true);
         await sleep(this_long);
+        toggle.value.active = false;
         toggle.value.disabled = false;
         disable(buttons, false);
         triggering_button.value.active = false;
     }
     
     function sleep(ms) {
-        return new Promise(resolve => timer = setTimeout(resolve, ms));
+        return new Promise(resolve =>  timer = setTimeout(resolve, ms));
     }
 
     function disable(array, toggle) {
